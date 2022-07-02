216212cbc3b8e29ac24e1e5a3cb0de690c8fe913
call1800messiah <>
2022-07-02 14:37:45 +0200

    Update to Angular 14

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'recipes'`, () => {
+  it(`should have as title 'home-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipes');
+    expect(app.title).toEqual('home-dashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('recipes app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('home-dashboard app is running!');
   });
 });
