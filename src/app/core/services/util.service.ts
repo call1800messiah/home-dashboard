7d6ebf495e243eb74e763828f49ad6c69e5f85eb
call1800messiah <>
2022-07-25 20:02:56 +0200

    Change name sorting to use localeCompare

--- src/app/core/services/util.service.ts
+++ src/app/core/services/util.service.ts
@@ -9,11 +9,15 @@ export class UtilService {
 
 
   static orderByName(a: { name: string }, b: { name: string }): 1 | -1 | 0 {
-    return UtilService.orderByObjectProperty(a, b, 'name', true);
+    return UtilService.orderByObjectProperty(a, b, 'name', true, true);
   }
 
 
-  private static orderByObjectProperty(a: any, b: any, property: string, asc: boolean): 1 | -1 | 0 {
+  private static orderByObjectProperty(a: any, b: any, property: string, asc: boolean, local: boolean = false): 1 | -1 | 0 {
+    if (local) {
+      return a[property].localeCompare(b[property], 'de-DE');
+    }
+
     if (a[property] < b[property]) {
       return asc ? -1 : 1;
     }
